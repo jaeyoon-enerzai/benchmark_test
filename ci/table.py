48a2a48540e96ec2c622200000aa025def999671
enerzai <enerzai@enerzai.com>
2024-05-16 17:15:53 +0900

    table format changed

--- ci/table.py
+++ ci/table.py
@@ -5,32 +5,30 @@ def create_table_header(commit, comm_same_group, comm_all_group):
     header = 'Model Name'
     num = 30
     strnum_list.append(num)
-    content += f'{header:^{num}}|'
+    commit = ''
+    content += f'{header:^{num}}<br>{commit}|'
     
     header = f'current commit'
     num = 25
     strnum_list.append(num)
-    strnum_list.append(num)
-    content += f'{header:^{num}}|'
+    commit = f'({commit[:20]})'
+    content += f'{header:^{num}}<br>{commit}|'
     
     header = f'Best under same group'
     num = 25
     strnum_list.append(num)
-    content += f'{header:^{num}}|'
+    commit = f'({comm_same_group[:20]})'
+    content += f'{header:^{num}}<br>{commit}|'
     
     header = f'Best under all groups'
     num = 25
     strnum_list.append(num)
-    content += f'{header:^{num}}|'
-    
-    content += '\n|'
-    commits = ['', f'({commit[:20]})', f'({comm_same_group[:20]})', f'({comm_all_group[:20]})']
-    
-    for c, n in zip(commits, strnum_list):
-        content += f'{c:^{n}}|'
+    commit = f'({comm_all_group[:20]})'
+    content += f'{header:^{num}}<br>{commit}|'
     
     content += '\n|'
     for n in strnum_list:
+        content += ':'
         content += '-' * n
-        content += '|'
+        content += ':|'
     return content, strnum_list
